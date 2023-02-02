cdbde4633cfe6a1a83d34fdd18c0c6571f353dab
CornelisZoutewelle <112868870+CornelisZoutewelle@users.noreply.github.com>
2023-02-02 12:39:30 +0100

    Add files via upload

---
+++ Opdracht_1.0 - 4.0 (ontwerpen)/action_page.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "U bent ingelogd!";
+echo "<br>";
+echo "<br>";
+echo $_POST["uname"];
+echo $_POST["psw"];
+echo "<br>";
+echo "<br>";
+echo "Today is " . date("Y/m/d") . "</br>";
+?>
