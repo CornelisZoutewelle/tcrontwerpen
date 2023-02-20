f5d8f4fb4923b72d686df8fc08efb90453c0a2ff
CornelisZoutewelle <112868870+CornelisZoutewelle@users.noreply.github.com>
2023-02-20 12:56:23 +0100

    Add files via upload

---
+++ Opdrachten_9.0/main_bieren.php
@@ -0,0 +1,26 @@
+<?php
+// Functie: Overzicht bieren.
+// Auteur: Cornelis Zoutewelle
+
+// Initialisatie.
+include 'functions.php';
+
+// Connect Database.
+ConnectDb();
+
+// Printen overzicht van bieren.
+OvzBieren();
+
+?>
+
+<!----- HTML Table Style ----->
+<style type="text/css">
+    table {
+        border-collapse: collapse;
+        border: 1px solid black;
+    }
+    td {
+        border: 1px solid black;
+        width: 100px;
+}
+</style>
