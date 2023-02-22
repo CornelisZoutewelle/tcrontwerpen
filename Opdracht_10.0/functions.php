5a89f8358e54e996ebc1dbc45ded4c38e965485c
CornelisZoutewelle <112868870+CornelisZoutewelle@users.noreply.github.com>
2023-02-22 15:11:21 +0100

    Opdracht 10
    Dit is opdracht 10 met de extra opdracht voor als je de eerdere opdracht al af had.

---
+++ Opdracht_10.0/functions.php
@@ -0,0 +1,67 @@
+<?php
+// Functie: Algemene functies voor 'main_brouwers.php'.
+// Auteur: Cornelis Zoutewelle
+
+// Function 'ConnectDb' connect de database.
+function ConnectDb(){
+    try{
+        $conn = new PDO("mysql:host=localhost;dbname=bieren", "root", "");
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        echo "Connected!";
+    } 
+    
+    catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+    return $conn;
+}
+
+// Function 'GetData' haalt de data uit de database.
+function GetData($table){
+    $conn = ConnectDb();
+
+    $query = $conn->prepare("SELECT * FROM $table");
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+// Function 'Table' weergeeft de data die je opvraagd.
+function PrintTable($result){
+    echo "<table border = 1px>";
+    foreach ($result as $row) {
+        echo "<tr>";
+        foreach ($row as $cell) {
+            echo "<td>" . $cell . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+// Function 'GetBrouwers' selecteert data van brouwers.
+function GetBrouwers(){
+    $result = GetData("brouwer");
+
+    echo "Overzicht van alle brouwers: ";
+
+    echo "<br>";
+    echo "<br>";
+
+    PrintTable($result);
+}
+
+// Function 'GetBieren' selecteert data van bieren.
+function GetBieren(){
+    $result = GetData("bier");
+
+    echo "Overzicht van alle bieren: ";
+
+    echo "<br>";
+    echo "<br>";
+
+    PrintTable($result);
+}
+
+?>
