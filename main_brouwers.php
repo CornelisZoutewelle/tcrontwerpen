374a4efce39a821ae989668d9cdcd76c16f2d08b
CornelisZoutewelle <112868870+CornelisZoutewelle@users.noreply.github.com>
2023-03-06 14:12:03 +0100

    Opdracht 11

---
+++ main_brouwers.php
@@ -0,0 +1,12 @@
+<?php
+// Functie: Overzicht brouwers.
+// Auteur: Cornelis Zoutewelle
+
+// Initialisatie.
+include 'functions.php';
+
+// Printen overzicht van bieren.
+GetBrouwers();
+//GetBieren();
+
+?>
